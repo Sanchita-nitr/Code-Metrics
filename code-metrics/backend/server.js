382322d29bf1a230dd146ccf12f54283896a1bc1
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-02-05 17:22:26 +0530

    final commit

--- code-metrics/backend/server.js
+++ code-metrics/backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const authRoutes = require("../routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
